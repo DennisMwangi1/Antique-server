2b73beadc49dae1008384760d4b5bda733f624fa
DennisMwangi1 <dennis.kamau@student.moringaschool.com>
2022-09-06 14:31:56 +0300

    added routes to fetch data from the database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,20 @@ class ApplicationController < Sinatra::Base
 
   end
 
+  post '/users' do
+    new_user=User.create(
+      first_name:params[:first_name],
+      last_name:params[:last_name],
+      user_name:params[:user_name],
+      email:params[:email],
+      password:params[:password],
+      gender:params[:gender],
+      age:params[:age]
+    )
+    new_user.to_json
+    
+  end
+
   get '/users/:id' do
     user = User.find(params[:id])
     user.to_json
